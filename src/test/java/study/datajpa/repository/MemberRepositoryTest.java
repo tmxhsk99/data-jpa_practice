563e966aa88bd7da0daa9ca27b9f69b1873bfd0e
kimjuhyeon <tmxhsk99@naver.com>
2022-08-24 09:02:57 +0900

    feat : 스프링 데이터 JPA 페이징과 정렬 관련 기능 및 테스트 코드 작성

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.Param;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -158,5 +161,44 @@ class MemberRepositoryTest {
         Optional<Member> optionalMember = memberRepository.findOptionalByUsername("memberAA");
         assertThat(optionalMember.get()).isEqualTo(memberAA);
 
+    }
+
+    @Test
+    public void paging() throws Exception {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 10));
+        memberRepository.save(new Member("member3", 10));
+        memberRepository.save(new Member("member4", 10));
+        memberRepository.save(new Member("member5", 10));
+
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC,"username"));
+
+        //when
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+
+        //then
+        List<Member> content = page.getContent();
+        long totalElements = page.getTotalElements();
+
+        for (Member member : content) {
+            System.out.println("member = " + member);
+        }
+        System.out.println("totalElements :"  + totalElements);
+
+        //페이지에서 불러오는 사이즈 검증
+        assertThat(content.size()).isEqualTo(3);
+        //totalCount 체크
+        assertThat(page.getTotalElements()).isEqualTo(5);
+        //페이지 넘버도 불러올 수 있다
+        assertThat(page.getNumber()).isEqualTo(0);
+        //총 페이지 수
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        //첫번째 페이지 인지
+        assertThat(page.isFirst()).isTrue();
+        //다음 페이지가 있는지
+        assertThat(page.hasNext()).isTrue();
     }
 }
